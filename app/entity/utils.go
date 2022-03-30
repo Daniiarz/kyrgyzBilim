73030c6765e2251bcb5368a9cc6990ecd9a3eb32
daniiar <daniyarflash.m01@gmail.com>
2022-03-30 21:58:50 +0600

    Refactored

--- app/entity/utils.go
+++ app/entity/utils.go
@@ -2,12 +2,12 @@ package entity
 
 import (
 	"fmt"
-	"github.com/joho/godotenv"
 	"os"
 )
 
 func SetMediaUrl(s string) string {
-	_ = godotenv.Load("/usr/src/app/.env")
-	url := fmt.Sprintf("%v/%v", os.Getenv("MEDIA_URL"), s)
+	mediaUrl := os.Getenv("MEDIA_URL")
+	fmt.Println(mediaUrl)
+	url := fmt.Sprintf("%v/%v", mediaUrl, s)
 	return url
 }

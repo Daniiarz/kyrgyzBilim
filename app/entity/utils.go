b9486c5bb62f0c89e18ee81ccb2f592dae39bc48
daniiar <daniyarflash.m01@gmail.com>
2022-03-30 22:04:31 +0600

    Refactored

--- app/entity/utils.go
+++ app/entity/utils.go
@@ -2,12 +2,13 @@ package entity
 
 import (
 	"fmt"
+	"log"
 	"os"
 )
 
 func SetMediaUrl(s string) string {
 	mediaUrl := os.Getenv("MEDIA_URL")
-	fmt.Println(mediaUrl)
+	log.Println(mediaUrl)
 	url := fmt.Sprintf("%v/%v", mediaUrl, s)
 	return url
 }
